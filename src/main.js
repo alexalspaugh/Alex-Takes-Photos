ad612cd34244ffaee7de109768d07759f49bdd3a
alexalspaugh <alexalspaugh@me.com>
2021-12-30 17:54:39 -0800

    added svg icons to toggle switch and cleaned up toggle styling

--- src/main.js
+++ src/main.js
@@ -1,20 +1,32 @@
 const toggle = document.querySelector(".toggle");
 const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
 const rootElement = document.querySelector("body");
+const svg = document.querySelector("svg")
 const sun = document.querySelector(".sun");
 const moon = document.querySelector(".moon");
 
 if (prefersDarkMode.matches) {
 	rootElement.classList.add("dark-theme");
-  moon.style.display = "block"
-  sun.style.display = "none"
+  moon.style.display="block"
+  sun.style.display="none"
 } else {
   rootElement.classList.remove("dark-theme");
-  moon.style.display = "none"
-  sun.style.display = "block"
+  moon.style.display="none"
+  sun.style.display="block"
+}
+
+function hideShow () {
+  if (moon.style.display === "block") {
+    moon.style.display="none"
+    sun.style.display="block"
+  } else {
+    moon.style.display="block"
+    sun.style.display="none"
+  }
 }
 
 function modeSwitch() {
+  hideShow()
   rootElement.classList.toggle("dark-theme");
 }
 
