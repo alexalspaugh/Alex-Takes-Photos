98c770b76c0c3308b5c6fc27862dedd8caedcaf8
alexalspaugh <alexalspaugh@me.com>
2021-12-30 16:46:03 -0800

    added svgs to toggle button

--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
 const toggle = document.querySelector(".toggle");
 const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
 const rootElement = document.querySelector("body");
+const sun = document.querySelector(".sun");
+const moon = document.querySelector(".moon");
 
 if (prefersDarkMode.matches) {
 	rootElement.classList.add("dark-theme");
+  moon.style.display = "block"
+  sun.style.display = "none"
 } else {
   rootElement.classList.remove("dark-theme");
+  moon.style.display = "none"
+  sun.style.display = "block"
 }
 
 function modeSwitch() {
-  
   rootElement.classList.toggle("dark-theme");
 }
 
